056901187203fdc1155cc0ac49a19225eaa2aac8
violet666666 <setanhantu11111@gmail.com>
2022-12-11 15:26:20 +0700

    menambahkan halaman artikel

---
+++ src/component/Article/CardImage.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const ImageCard = styled.img`
+    background-color: #38FF88;
+    color: #fff;
+    padding: 30px;
+    max-width: 35%;
+`;
+export default ImageCard;
--- src/page/Article/index.js
+++ src/page/Article/index.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import CardContent from '../../component/Article/CardContent';
+import Wrapper from '../../component/Article/Wrapper';
+import Description from '../../component/Article/CardDescription';
+import ImageCard from '../../component/Article/CardImage';
+import CardInfo from '../../component/Article/CardInfo';
+import ButtonCard from '../../component/Article/CardButton';
+import article from '../../component/Article/local-data'
+import './index.css';
 
-const Article = () => {
+function Articles() {
     return (
-        <div>
-            <h1>index</h1>
-        </div>
-    )
+    <section className="Articles">
+        {article.map((article) => (
+            <Wrapper className='Card-Wrapper' key={article.id} tabIndex='0'>
+                <CardContent className='Card-Content' key={article.id} tabIndex='0'>
+                    <ImageCard src={article.image} tabIndex='0'></ImageCard>
+                <CardInfo className='Card-Info' tabIndex='0'>
+                    <h2 tabIndex='0'>{article.title}</h2>
+                    <Description className='Description' tabIndex='0'>{article.body}</Description>
+                    
+                    <ButtonCard className='Button-Card' tabIndex='0'><a href={article.source} target="blank">Baca Selengkapnya<span>&rarr;</span></a></ButtonCard>
+                    
+                </CardInfo>
+                </CardContent>
+            </Wrapper>
+        ))
+        }
+    </section>
+    );
 }
 
-export default Article
+export default Articles;
